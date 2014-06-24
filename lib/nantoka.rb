d9d73510b80786ccc726c84c2c6b9d8b9ccdb8ac
dtan4 <dtanshi45@gmail.com>
2014-06-24 15:15:15 +0900

    Replace to なんとか

--- lib/nantoka.rb
+++ lib/nantoka.rb
@@ -1,5 +1,41 @@
 require "nantoka/version"
+require "natto"
 
 module Nantoka
-  # Your code goes here...
+  class << self
+    def nantoka(text)
+      raise ArgumentError if text.nil? || !text.is_a?(String)
+      replace_to_nantoka(text)
+    end
+
+    private
+
+    def first_noun_index_of(parse_result)
+      parse_result.each_with_index do |word, index|
+        return index if word[:type] == "名詞"
+      end
+
+      return 0
+    end
+
+    def parse(text)
+      result = []
+
+      Natto::MeCab.new.parse(text) do |n|
+        result << { surface: n.surface, type: type(n.feature) } if n.surface
+      end
+
+      result
+    end
+
+    def replace_to_nantoka(text)
+      parse_result = parse(text)
+      parse_result[first_noun_index_of(parse_result)][:surface] = "なんとか"
+      parse_result.map { |w| w[:surface] }.join("")
+    end
+
+    def type(feature)
+      feature.split(",")[0]
+    end
+  end
 end
--- spec/nantoka_spec.rb
+++ spec/nantoka_spec.rb
@@ -1,11 +1,67 @@
 require 'spec_helper'
 
 describe Nantoka do
-  it 'has a version number' do
+  it "has a version number" do
     expect(Nantoka::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "#nantoka" do
+    let(:nantoka) do
+      Nantoka.nantoka(arg)
+    end
+
+    context "nil is given" do
+      let(:arg) do
+        nil
+      end
+
+      it "should raise ArgumentError" do
+        expect do
+          nantoka
+        end.to raise_error ArgumentError
+      end
+    end
+
+    context "non-String is given" do
+      let(:arg) do
+        ["a"]
+      end
+
+      it "should raise ArgumentError" do
+        expect do
+          nantoka
+        end.to raise_error ArgumentError
+      end
+    end
+
+    context "noun is given" do
+      let(:arg) do
+        "システム"
+      end
+
+      it "should replace it" do
+        expect(nantoka).to eq "なんとか"
+      end
+    end
+
+    context "compound is given" do
+      let(:arg) do
+        "テスト駆動開発"
+      end
+
+      it "should replace it" do
+        expect(nantoka).to eq "なんとか駆動開発"
+      end
+    end
+
+    context "sentence is given" do
+      let(:arg) do
+        "飯を食べる"
+      end
+
+      it "should replace it" do
+        expect(nantoka).to eq "なんとかを食べる"
+      end
+    end
   end
 end
